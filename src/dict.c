9b82373e49d993aecd6858336436f7031d80f115
zlwgx <389179924@qq.com>
2016-11-23 22:40:01 +0800

    Add dict

---
+++ src/dict.c
@@ -0,0 +1,109 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+#include "dict.h"
+
+#define DICT_INIT_SIZE 8
+
+static size_t hash(const char *s)
+{
+    size_t r = 2166136261;
+    for (; *s; s++) {
+        r ^= *s;
+        r *= 16777619;
+    }
+    return r;
+}
+
+dict_t *make_dict(void)
+{
+    dict_t *dict = malloc(sizeof(*dict));
+    dict->link = NULL;
+    dict->used = 0;
+    dict->mask = DICT_INIT_SIZE - 1;
+    dict->table = calloc(DICT_INIT_SIZE, sizeof(dict_entry_t));
+    return dict;
+}
+
+#define PROBE(i) ((i) + 1)
+
+static dict_entry_t *lookup(dict_t *dict, const char *key, size_t h)
+{
+    size_t i;
+    dict_entry_t *e;
+
+    for (i = h & dict->mask; ; i = PROBE(i) & dict->mask) {
+        e = &dict->table[i];
+        if (!e->key || (e->hash == h && !strcmp(e->key, key)))
+            return e;
+    }
+}
+
+void *dict_lookup(dict_t *dict, const char *key)
+{
+    dict_entry_t *e;
+
+    assert(dict && key);
+    e = lookup(dict, key, hash(key));
+    return e->key ? e->val : NULL;
+}
+
+static void dict_resize(dict_t *dict, size_t new_size)
+{
+    dict_entry_t *e, *old = dict->table;
+    dict->table = calloc(new_size, sizeof(dict_entry_t));
+    dict->mask = new_size - 1;
+    size_t i = dict->used;
+    dict->used = 0;
+
+    for (e = old; i > 0; e++) {
+        if (e->key) {
+            dict_insert(dict, e->key, e->val, 0);
+            i--;
+        }
+    }
+}
+
+int dict_insert(dict_t *dict, char *key, void *val, int flag)
+{
+    dict_entry_t *e;
+    size_t h;
+
+    assert(dict && key);
+    h = hash(key);
+    e = lookup(dict, key, h);
+    if (!e->key) {
+        e->hash = h;
+        e->key = key;
+        e->val = val;
+        dict->used++;
+    } else {
+        if (flag)
+            return 0;
+        else
+            e->val = val;
+    }
+
+    if (dict->used * 3 >= (dict->mask + 1) * 2)
+        dict_resize(dict, (dict->mask + 1) * 2);
+    return 1;
+}
+
+void dict_free(dict_t *dict, void (*free_key)(char *), void (*free_val)(void *))
+{
+    dict_entry_t *e;
+
+    assert(dict);
+    for (e = dict->table; dict->used > 0; e++) {
+        if (e->key) {
+            if (free_key)
+                (*free_key)(e->key);
+            if (free_val)
+                (*free_val)(e->val);
+            dict->used--;
+        }
+    }
+    free(dict->table);
+    free(dict);
+}
---
+++ src/dict.h
@@ -0,0 +1,28 @@
+#ifndef DICT_H__
+#define DICT_H__
+
+#include <stddef.h> /* for size_t */
+
+typedef struct dict_entry_t {
+    size_t hash;
+    char *key;
+    void *val;
+} dict_entry_t;
+
+typedef struct dict_t {
+    struct dict_t *link;
+    size_t used;
+    size_t mask;
+    dict_entry_t *table;
+} dict_t;
+
+dict_t *make_dict(void);
+void *dict_lookup(dict_t *dict, const char *key);
+/* flag == 1: insert if key is not in dict, if key is in dict, return 0
+ * flag == 0: insert no matter whether key is in dict
+ * succecc return 1
+ */
+int dict_insert(dict_t *dict, char *key, void *val, int flag);
+void dict_free(dict_t *dict, void (*free_key)(char *), void (*free_val)(void *));
+
+#endif
